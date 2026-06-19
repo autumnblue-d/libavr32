4c29382438167c3bc0872e2c2f84b0d3616eeb1a
Daanyal Baber <awesomebrick@hotmail.com>
2026-06-19 12:02:19 -0500

    keyboard fix (#79)
    * updated dirty flag to be a u64, to match hid_max_frame_bytes
    
    * changed 1 to 1ULL. fixes a bunch of keyboards not working.
    
    ---------
    
    Co-authored-by: awesomebrick <baberd@slu.edu>

--- src/usb/hid/hid.c
+++ src/usb/hid/hid.c
@@ -30,7 +30,7 @@ static u8 frame[HID_FRAME_MAX_BYTES] = {
   0x00,   0x00,   0x00,   0x00, 
 };
 
-static u32 dirty = 0xffffffff;
+static u64 dirty = 0xffffffffffffffff;
 static u8 size = HID_FRAME_MAX_BYTES;
 
 // app polls
@@ -38,17 +38,19 @@ static u8 size = HID_FRAME_MAX_BYTES;
 
 // set/unset dirty flag for given byte in frame
 static inline void hid_set_byte_flag(u8 byte, u8 val) {
-  if(val) {
-    dirty |= (1 << byte);
-  } else {
-    dirty &= 0xffffffff ^ (1 << byte);
+  if (byte<64) { //bytecount guard
+    if(val) {
+      dirty |= (1ULL << byte); //needs to be ULL. "1" is compiled as u32 int, so shifting > 32 is undefined
+    } else {
+      dirty &= ~(1ULL << byte);
+    }
   }
 }
 
 // test dirty flag for given byte in packet
 //u8 hid_get_byte_flag(u32 data, u8 byte) {
 u8 hid_get_byte_flag(u8 byte) {
-  return (dirty & (1 << byte)) > 0;
+  return (dirty & (1ULL << byte)) > 0;
 }
 
 // parse frame and spawn events
@@ -88,8 +90,8 @@ const volatile u8 hid_get_frame_size(void) {
   return (const volatile u8)size;
 }
 
-const volatile u32 hid_get_frame_dirty(void) {
-  return (const volatile u32)dirty;
+const volatile u64 hid_get_frame_dirty(void) {
+  return (const volatile u64)dirty;
 }
 
 // HID device was plugged or unplugged
@@ -107,7 +109,7 @@ extern void hid_change(uhc_device_t* dev, u8 plug) {
 
 // clear the bitfield of dirty bytes
 extern void hid_clear_frame_dirty(void) {
-  dirty = 0x00000000;
+  dirty = 0x0000000000000000;
 }
 
 #include "uhc.h"
